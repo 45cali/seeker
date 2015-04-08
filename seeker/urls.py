de359bfd226724bfee22382a49f4b551f03334c9
Nicholas Colbert <nicholas.colbert@ticketmaster.com>
2015-04-08 15:51:37 -0700

    added root path

--- seeker/urls.py
+++ seeker/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from seeker.rest import apiviews
+from django.views.generic.base import TemplateView
 
 
 router = routers.SimpleRouter(trailing_slash=True)
@@ -23,6 +24,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^$', TemplateView.as_view(template_name = "index.html"), name='index'),
 
 )
 
