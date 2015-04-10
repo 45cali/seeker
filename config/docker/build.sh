f6f644fd85cf22bd90667106d1bebbcd80d6ef45
Nicholas Colbert <nicholas.colbert@ticketmaster.com>
2015-04-10 13:56:54 -0700

    docker update

--- config/docker/build.sh
+++ config/docker/build.sh
@@ -2,4 +2,5 @@ curl https://raw.githubusercontent.com/45cali/seeker/master/config/docker/Docker
 
 sudo docker build -t='seeker:latest' .
 
-sudo docker run -i -t='seeker:latest' -p 80:80 /usr/sbin/apache2 -D FOREGROUND
+sudo docker run -d -i -t -P seeker:latest /usr/sbin/apache2ctl -D FOREGROUND
+
