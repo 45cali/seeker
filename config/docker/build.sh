3731b4bcf6640ddb49268fe34cf3712133be2d88
45cali <45cali@gmail.com>
2015-04-17 15:15:28 -0700

    Update build.sh

--- config/docker/build.sh
+++ config/docker/build.sh
@@ -1,6 +1,6 @@
 curl https://raw.githubusercontent.com/45cali/seeker/mysql/config/docker/Dockerfile > ./Dockerfile
 
-sudo docker build -t='seeker:mysqlv1' .
+sudo docker build -t='seeker:1.0' .
 
-sudo docker run -d -i -t -P seeker:mysqlv1 /usr/sbin/apache2ctl -D FOREGROUND
+sudo docker run -d -i -t -P seeker:1.0 /usr/sbin/apache2ctl -D FOREGROUND
 
