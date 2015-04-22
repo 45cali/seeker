ec20228d72e271986c8f04a45103fb666559d986
45cali <45cali@gmail.com>
2015-04-22 09:43:56 -0700

    updated settings.py

--- seeker/settings.py
+++ seeker/settings.py
@@ -105,9 +105,9 @@ REST_FRAMEWORK = {
 				  'rest_framework.renderers.BrowsableAPIRenderer', ),    
 
      'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
